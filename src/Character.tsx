3a3c5ee7a5c2e09dce5ff7b0f8e59f626e364b14
thanhduong <xuanthanhduong94@gmail.com>
2022-12-14 18:19:25 +0100

    feat: searchbar and hook

--- src/Character.tsx
+++ src/Character.tsx
@@ -1,18 +1,7 @@
-type Character ={
+type Character = {
     id: number;
     name: string;
-    species:string;
     status: string;
-    type:string;
-    gender: string;
-    origin: {   name: string;
-        url: string; };
-    location:{name: string;
-        url: string; };
-    image:string;
-    episode: string[];
-    url:string;
-    created: string;
+    image: string;
 }
-
 export default Character;
--- src/CharacterGallery.tsx
+++ src/CharacterGallery.tsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import Character from "./Character";
 import CharacterCard from "./CharacterCard";
 
-export default function CharacterGallery({characters} : { characters: Character[] }){
+export default function CharacterGallery(props : { characters: Character[] }){
 
+    const [name, setName]= useState<string>("");
     return <div className={"cards-container"}>
+                <input onChange={(event) =>setName(event.target.value)}/>
+                <br/>
                 <div className="cards">
-                    {characters.map(character => (<CharacterCard
+                    {props.characters.filter(character => character.name.includes(name))
+                        .map(character => (<CharacterCard
                         key={character.id}
                         name={character.name}
                         image={character.image}
